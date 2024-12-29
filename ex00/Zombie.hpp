be08281fc896b55c0ea9af74fa966071124f591a
Collignon Nicolas <nicolas.collignon7@gmail.com>
2024-12-29 13:33:01 +0100

    ex00

---
+++ ex00/Zombie.hpp
@@ -0,0 +1,22 @@
+#ifndef ZOMBIE_HPP
+#define ZOMBIE_HPP
+
+#include <string>
+#include <iostream>
+
+// Définition de la classe Zombie
+class Zombie {
+private:
+    std::string name;
+
+public:
+    Zombie(std::string name);
+    ~Zombie();
+
+    void announce() const;
+};
+
+Zombie* newZombie(std::string name);
+void randomChump(std::string name);
+
+#endif
